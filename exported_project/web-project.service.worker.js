685529d38af56721317fd4f78dd12cd55d6254e0
Warren Scipio <warrenscipio@gmail.com>
2025-01-21 18:13:47 -0800

    updated export to 1876dde

--- exported_project/web-project.service.worker.js
+++ exported_project/web-project.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1736550129|290557326';
+const CACHE_VERSION = '1737511835|95470315';
 /** @type {string} */
 const CACHE_PREFIX = 'web-project-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
