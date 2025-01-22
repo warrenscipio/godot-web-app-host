490ef26099a03487a7c6dd6b0102080db0f09d21
Warren Scipio <warrenscipio@gmail.com>
2025-01-21 19:16:46 -0800

    exported a build trying to get fetch to localstorage

--- exported_project/web-project.service.worker.js
+++ exported_project/web-project.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1737511835|95470315';
+const CACHE_VERSION = '1737515719|3589334070';
 /** @type {string} */
 const CACHE_PREFIX = 'web-project-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
