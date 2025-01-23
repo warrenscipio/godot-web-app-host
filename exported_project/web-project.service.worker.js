922497ada3755df60c099d34e8fef1c181015b0e
Warren Scipio <warrenscipio@gmail.com>
2025-01-23 15:38:53 -0800

    I need a better way to do this

--- exported_project/web-project.service.worker.js
+++ exported_project/web-project.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1737675006|13290222097';
+const CACHE_VERSION = '1737675457|13741590039';
 /** @type {string} */
 const CACHE_PREFIX = 'web-project-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
