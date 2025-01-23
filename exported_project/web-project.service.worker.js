703782d09fa5cab0ddd58bb834a4d579ea4e66d8
Warren Scipio <warrenscipio@gmail.com>
2025-01-23 15:43:25 -0800

    all I changed was using single quotes vs double quotes

--- exported_project/web-project.service.worker.js
+++ exported_project/web-project.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1737675457|13741590039';
+const CACHE_VERSION = '1737675713|13997411299';
 /** @type {string} */
 const CACHE_PREFIX = 'web-project-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
