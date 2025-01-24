1df11a0449c374a5d0ce4312d7de495df276d2d3
Warren Scipio <warrenscipio@gmail.com>
2025-01-23 20:22:29 -0800

    Finally this is working, the trick was there is an instance of localStorage that exists in the iframe that I cant access by just going to the iframes link..

--- exported_project/web-project.service.worker.js
+++ exported_project/web-project.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1737678864|17148351698';
+const CACHE_VERSION = '1737692467|30749540372';
 /** @type {string} */
 const CACHE_PREFIX = 'web-project-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
