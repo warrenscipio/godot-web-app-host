5bc73ac3144092734135b17252bda164e9d98fa9
Warren Scipio <warrenscipio@gmail.com>
2025-01-25 12:47:32 -0800

    we are testing what retrieved data is before returned

--- exported_project/web-project.service.worker.js
+++ exported_project/web-project.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1737706673|44956203743';
+const CACHE_VERSION = '1737837975|2178607436';
 /** @type {string} */
 const CACHE_PREFIX = 'web-project-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
