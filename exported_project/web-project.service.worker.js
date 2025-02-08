d975821ad0ec0b1b43e1093799c3bd1941c4de32
Warren Scipio <warrenscipio@gmail.com>
2025-02-08 15:36:03 -0800

    added the logo to the splash screen

--- exported_project/web-project.service.worker.js
+++ exported_project/web-project.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1739056696|4816486572';
+const CACHE_VERSION = '1739057730|5850511238';
 /** @type {string} */
 const CACHE_PREFIX = 'web-project-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
