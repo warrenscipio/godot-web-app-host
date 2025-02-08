f38cd1526831b07b2e4958a2acd4185a935b9238
Warren Scipio <warrenscipio@gmail.com>
2025-02-08 15:18:59 -0800

    messed with the viewport to make it look like the background was white

--- exported_project/web-project.service.worker.js
+++ exported_project/web-project.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1738965936|693813057';
+const CACHE_VERSION = '1739056696|4816486572';
 /** @type {string} */
 const CACHE_PREFIX = 'web-project-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
